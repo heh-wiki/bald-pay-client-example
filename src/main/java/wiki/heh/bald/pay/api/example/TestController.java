4d439c064e7770e898314917a692105012aea436
kugs <5409290@qqcom>
2020-12-07 15:36:03 +0800

    支付宝APP支付及支付宝二维码支付示例

--- src/main/java/wiki/heh/bald/pay/api/example/TestController.java
+++ src/main/java/wiki/heh/bald/pay/api/example/TestController.java
@@ -2,9 +2,14 @@ package wiki.heh.bald.pay.api.example;
 
 import net.mfeng.pay.api.PayClient;
 import net.mfeng.pay.api.model.AliPayQrParam;
+import net.mfeng.pay.api.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author hehua
@@ -15,23 +20,51 @@ public class TestController {
     @Autowired
     PayClient client;
 
-    @GetMapping("notifyUrl")
-    public void notifyUrl(Object o) {
+    @PostMapping("notifyUrl")
+    public String notifyUrl(Object o) {
         System.out.println("收到支付中心的回调信息");
         System.out.println(o.toString());
+        return "success";
     }
 
     @GetMapping("test")
-    public void test() {
+    public String test() {
+        AtomicLong seq = new AtomicLong(0L);
+        String goodsOrderId = String.format("%s%s%06d", "G", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()), (int) seq.getAndIncrement() % 1000000);
         AliPayQrParam param = new AliPayQrParam();
-        param.setMchId("10000");
-        param.setMchOrderNo("12332444444");
+        param.setMchId("10000000");
+        param.setMchOrderNo(goodsOrderId);
         param.setAmount("11110");
         param.setClientIp("192.168.1.22");
         param.setDevice("android");
         param.setSubject("这里是标题");
         param.setBody("这里是商品介绍呀");
-        System.out.println(client.aliPayQr(param));
+        String s = client.aliPayQr(param);
+        System.out.println();
+        return s;
+    }
+
+    @PostMapping("test1")
+    public String test11(@RequestBody TestUnifiedPayForm fee) {
+        return testm(fee.getFee());
+    }
+    @GetMapping("test1")
+    public String testm(String fee) {
+        if(StringUtil.isNotEmpty(fee)) {
+            AtomicLong seq = new AtomicLong(0L);
+            String goodsOrderId = String.format("%s%s%06d", "G", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()), (int) seq.getAndIncrement() % 1000000);
+            AliPayQrParam param = new AliPayQrParam();
+            param.setMchId("10000000");
+            param.setMchOrderNo(goodsOrderId);
+            param.setAmount(fee);
+            param.setClientIp("192.168.1.22");
+            param.setDevice("android");
+            param.setSubject("这里是标题");
+            param.setBody("这里是商品介绍呀");
+            String s = client.aliPayMobile(param);
+            return s;
+        }
+        return TestResult.file("支付金额不能为空").toString();
     }
 
 }
---
+++ src/main/java/wiki/heh/bald/pay/api/example/TestUnifiedPayForm.java
@@ -0,0 +1,28 @@
+package wiki.heh.bald.pay.api.example;
+
+/**
+ * @author hehua
+ * @date 2020/12/4
+ */
+public class TestUnifiedPayForm {
+
+
+    int type;
+    String fee;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getFee() {
+        return fee;
+    }
+
+    public void setFee(String fee) {
+        this.fee = fee;
+    }
+}
