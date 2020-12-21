fc0fa180c67ac6151157209e47b1121282e716af
kugs <5409290@qqcom>
2020-12-21 15:57:07 +0800

    订单退款的实现

--- src/main/java/wiki/heh/bald/pay/api/example/TestController.java
+++ src/main/java/wiki/heh/bald/pay/api/example/TestController.java
@@ -1,7 +1,10 @@
 package wiki.heh.bald.pay.api.example;
+import net.mfeng.pay.api.model.ChannelEnum;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import net.mfeng.pay.api.PayClient;
 import net.mfeng.pay.api.model.AliPayQrParam;
+import net.mfeng.pay.api.model.UnifiedRefundParam;
 import net.mfeng.pay.api.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -21,10 +26,29 @@ public class TestController {
     PayClient client;
 
     @PostMapping("notifyUrl")
-    public String notifyUrl(Object o) {
+    public String notifyUrl(@RequestParam Map o) {
         System.out.println("收到支付中心的回调信息");
         System.out.println(o.toString());
         return "success";
+    }
+
+    @GetMapping("refund")
+    public String refund() {
+        UnifiedRefundParam param = new UnifiedRefundParam();
+        param.setPayOrderId("P0020201221111507000001");
+        param.setMchRefundNo("RR2020120221060400053119");
+        param.setChannelId(ChannelEnum.ALIPAY_QR);
+        param.setChannelUser("支付宝帐号");
+        param.setUserName("张三李四都可以");
+        param.setRemarkInfo("取消订单");
+        param.setMchId("10000000");
+        param.setMchOrderNo("2020120221060400052");
+        param.setAmount("10");
+        param.setClientIp("192.168.1.22");
+        param.setDevice("android");
+        String s = client.tradeRefund(param);
+        System.out.println();
+        return s;
     }
 
     @GetMapping("test")
@@ -46,11 +70,12 @@ public class TestController {
 
     @PostMapping("test1")
     public String test11(@RequestBody TestUnifiedPayForm fee) {
-        return testm(fee.getFee());
+        return aliPayMobile(fee.getFee());
     }
-    @GetMapping("test1")
-    public String testm(String fee) {
-        if(StringUtil.isNotEmpty(fee)) {
+
+    @GetMapping("aliPayMobile")
+    public String aliPayMobile(String fee) {
+        if (StringUtil.isNotEmpty(fee)) {
             AtomicLong seq = new AtomicLong(0L);
             String goodsOrderId = String.format("%s%s%06d", "G", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()), (int) seq.getAndIncrement() % 1000000);
             AliPayQrParam param = new AliPayQrParam();
